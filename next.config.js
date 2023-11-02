e41b39b5e3ae2ab318e18dec225c5d00c66107f5
berkayoruc <berkayoruc@hotmail.com>
2023-11-03 00:06:18 +0300

    next  config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,6 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  staticPageGenerationTimeout: 180,
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
