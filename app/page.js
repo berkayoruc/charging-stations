bfd340288447b0aca24751f2411825a1729db52f
berkayoruc <berkayoruc@hotmail.com>
2023-11-03 00:23:11 +0300

    fixed?

--- app/page.js
+++ app/page.js
@@ -12,20 +12,22 @@ async function getData() {
       url: "https://veri.kocaeli.bel.tr/tr/dataset/9021334f-358b-4a1a-9f4f-76592ca07541/resource/f853e162-8895-47e8-8bcf-e1b8796f1cf1/download/akulu-sandalye-arj-istasyonu.json",
       city: "kocaeli",
     },
-    // {
-    //   url: "https://acikveri.bizizmir.com/api/3/action/datastore_search?resource_id=028f2692-d930-481f-ab27-17a321bd1283&limit=5",
-    //   city: "izmir",
-    // },
-    // {
-    //   url: "https://opendata.gaziantep.bel.tr/api/3/action/datastore_search?resource_id=a9b86fc1-af55-41ca-984e-164c161c14d3&limit=500",
-    //   city: "gaziantep",
-    // },
+    {
+      url: "https://acikveri.bizizmir.com/api/3/action/datastore_search?resource_id=028f2692-d930-481f-ab27-17a321bd1283&limit=500",
+      city: "izmir",
+    },
+    {
+      url: "https://opendata.gaziantep.bel.tr/api/3/action/datastore_search?resource_id=a9b86fc1-af55-41ca-984e-164c161c14d3&limit=500",
+      city: "gaziantep",
+    },
   ];
   let stations = { type: "FeatureCollection", features: [] };
-  try {
-    for (let i = 0; i < urls.length; i++) {
-      const { url, city, district } = urls[i];
-      const res = await axios.get(url);
+
+  for (let i = 0; i < urls.length; i++) {
+    const { url, city, district } = urls[i];
+
+    const res = await axios.get(url).catch((err) => err);
+    if (res.status === 200) {
       if (res && res.data) {
         if (city === "istanbul" && district === "tuzla") {
           stations.features = stations.features.concat(
@@ -58,19 +60,18 @@ async function getData() {
           );
         }
       }
+    } else {
+      console.log("error", res.status, res.statusText, city);
     }
-  } catch (error) {
-    console.log("noldu", error);
   }
   return stations;
 }
 
 export default async function Home() {
   const stations = await getData();
-  console.log(stations);
   return (
     <div className="h-screen">
-      <LocationAggregatorMap />
+      <LocationAggregatorMap stations={stations} />
     </div>
     // <ul>
     //   {stations.features.map((station) => (
